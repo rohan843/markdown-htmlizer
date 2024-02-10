eab8ce4da2427b3d7c15c3d29b4f09db0b0e2e72
rohan843 <rohan.sharma2934@gmail.com>
2024-02-10 16:37:45 +0530

    fix: setup input and output files

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
 #include "lex.yy.c"
+extern FILE *yyout;
 
 int main()
 {
-
+    FILE *inputFile = fopen("input.md", "r");
+    FILE *outputFile = fopen("output.html", "w");
+    yyrestart(inputFile);
+    yyout = outputFile;
+    yylex();
+    fclose(inputFile);
+    fclose(outputFile);
     return 0;
 }
